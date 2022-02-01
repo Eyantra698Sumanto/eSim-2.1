a8f303d588b5dcef52c021d1003a975179a42752
Eyantra698Sumanto <jeetsumanto123@gmail.com>
2022-02-01 16:13:36 +0530

    Edited createkicad

--- eSim-2.1/src/maker/createkicad.py
+++ eSim-2.1/src/maker/createkicad.py
@@ -96,7 +96,7 @@ class AutoSchematic:
             ret = QtWidgets.QMessageBox.critical(
                 self.parent, "Error", '''<b>A standard library already ''' +
                 '''exists with this name.</b><br/><b>Please change the ''' +
-                '''name of your vhdl file and upload it again</b>''',
+                '''name of your verilog file and upload it again</b>''',
                 QtWidgets.QMessageBox.Ok
             )
 
